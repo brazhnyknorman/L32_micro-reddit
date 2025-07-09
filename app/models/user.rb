8ef88a32b6355e72a5a5b2789ef24531804e54c8
brazhnyknorman <brazhnyknorman@gmail.com>
2025-07-09 18:52:33 -0400

    Implement User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :username, length: { minimum: 3, maximum: 60 }
+
+  validates :email, presence: true
+  validates :email, uniqueness: true
+
+  validates :password, presence: true
+  validates :password, length: { minimum: 6 }
 end
